65b035f1f41a02b793bfe982974d00f53213cd9b
Kale Evans <kaletevans@gmail.com>
2023-12-03 18:31:49 -0600

    Added functions for Principal Component Analysis, and plotted vs normal returns

--- FamaFrenchModel.py
+++ FamaFrenchModel.py
@@ -3,6 +3,7 @@ from datetime import datetime
 import pandas as pd
 import numpy as np
 import math
+import matplotlib.pyplot as plt
 
 stock_data = {}
 
@@ -137,11 +138,101 @@ print("=======================")
 mean_vector_of_returns = np.mean(returns_matrix, axis=0)
 # Then get the covariance matrix
 returns_cov_matrix = get_covariance_matrix(returns_matrix)
+
+print("Covariance Matrix of Returns")
+print(returns_cov_matrix)
+print("=======================")
+
 # Lambda is a diagonal matrix containing the eigenvalues of the covariance matrix
 # Find eigenvalues
 eigenvalues, eigenvectors = np.linalg.eig(returns_cov_matrix)
-print(np.sort(eigenvalues))
-print(eigenvectors)
 
+# We need to sort the eigenvalues from largest to smallest
+# Get the index values from the sort
+idx = eigenvalues.argsort()[::-1]
+sorted_eigenvalues = eigenvalues[idx]
+sorted_eigenvectors = eigenvectors[:, idx]
 
-    
+# Our gamma matrix is the sorted eigenvectors matrix
+gamma_matrix = sorted_eigenvectors
+print("Gamma Matrix")
+print(gamma_matrix)
+print("======================")
+# Create the lambda matrix
+lambda_matrix = np.array([[sorted_eigenvalues[0], 0, 0, 0],
+                          [0, sorted_eigenvalues[1], 0, 0],
+                          [0, 0, sorted_eigenvalues[2], 0],
+                          [0, 0, 0, sorted_eigenvalues[3]]])
+
+gamma_transpose = gamma_matrix.T
+
+# To get the principal components, we need to subtract the mean vector from each row of returns
+mean_subtracted_returns = []
+n = len(returns_matrix)
+for i in range(n):
+    r1 = returns_matrix[i, 0] - mean_vector_of_returns[0]
+    r2 = returns_matrix[i, 1] - mean_vector_of_returns[1]
+    r3 = returns_matrix[i, 2] - mean_vector_of_returns[2]
+    r4 = returns_matrix[i, 3] - mean_vector_of_returns[3]
+
+    mean_subtracted_returns.append([r1, r2, r3, r4])
+
+# Note the transpose, as each r should be represented as a row
+mean_subtracted_returns = np.asarray(mean_subtracted_returns).T
+
+# # Now we can find Y
+principal_components = gamma_transpose @ mean_subtracted_returns
+
+# We take the first two rows for Y1 and Y2
+# Now run a regression, using Y1 in Y2 as the F matrix, and the returns as the R
+# Create the F matrix with a preceding column of 1s
+PCA_F_Matrix = []
+Y1_Vector = []
+for i in range(principal_components.shape[1]):
+    value = principal_components[:, i]
+
+    F_data = np.array([1, value[0], value[1]])
+    PCA_F_Matrix.append(F_data)
+    Y1_Vector.append(value[0])
+
+PCA_F_Matrix = np.asarray(PCA_F_Matrix)
+PCA_F_Matrix_Transpose = PCA_F_Matrix.T
+Y1_Vector = np.asarray(Y1_Vector)
+
+# With the returns matrix as R, compute the regression
+PCA_F_Inverse = np.linalg.inv(PCA_F_Matrix_Transpose @ PCA_F_Matrix)
+PCA_B_Matrix = PCA_F_Inverse @ PCA_F_Matrix_Transpose @ returns_matrix
+
+print("B Matrix Obtained From PCA")
+print(PCA_B_Matrix)
+print("=======================")
+
+# Now we compute the residuals matrix
+PCA_Residuals_Matrix = returns_matrix - (PCA_F_Matrix @ PCA_B_Matrix)
+
+# Send to correlation function to retrieve correlation matrix
+PCA_Residuals_Correlation = get_correlation_matrix(PCA_Residuals_Matrix)
+PCA_Residuals_Covariance = get_covariance_matrix(PCA_Residuals_Matrix)
+
+print("PCA Factor Model Residuals Correlation Matrix")
+print(PCA_Residuals_Correlation)
+print("=====================")
+print("PCA Factor Model Residuals Covariance Matrix")
+print(PCA_Residuals_Covariance)
+
+# Now we wish to plot Y1 with the market returns
+market_returns = []
+for date in stock_data:
+    rm = stock_data[date]['mkt_rf'] + stock_data[date]['rf']
+    market_returns.append(rm)
+
+market_returns = np.asarray(market_returns)
+
+# Calculate correlation between Y1 and Market Returns
+print(np.corrcoef(Y1_Vector, market_returns))
+
+plt.scatter(Y1_Vector, market_returns)
+plt.xlabel('First Principal Component (Y1)')
+plt.ylabel('Market Return (rm)')
+plt.title('First Principal Component Vs Stock Market Return')
+plt.show()
